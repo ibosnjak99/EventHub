1c95e6572b44c726e5268ebc6c96c8ba84dee370
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2023-03-23 00:06:48 +0100

    Seeding the database

--- API/Program.cs
+++ API/Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-var app = builder.Build();
 builder.Services.RegisterDependencies();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,5 +28,20 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+}
 
 app.Run();
---
+++ Infrastructure/Seed.cs
@@ -0,0 +1,79 @@
+﻿using Domain;
+
+namespace Infrastructure
+{
+    /// <summary>
+    /// The seed class
+    /// </summary>
+    public class Seed
+    {
+        /// <summary>
+        /// Seeds the data.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Events.Any()) return;
+
+            var events = new List<Event>
+            {
+                new Event
+                {
+                    Title = "Past Event 1",
+                    Date = DateTime.UtcNow.AddMonths(-2),
+                    Description = "Old event",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Pub"
+                },
+                new Event
+                {
+                    Title = "Past Event 2",
+                    Date = DateTime.UtcNow.AddMonths(-1),
+                    Description = "Old event",
+                    Category = "food",
+                    City = "Paris",
+                    Venue = "Cafe"
+                },
+                new Event
+                {
+                    Title = "Past Event 3",
+                    Date = DateTime.UtcNow.AddMonths(-2),
+                    Description = "Old event",
+                    Category = "drinks",
+                    City = "Berlin",
+                    Venue = "Pub"
+                },
+                new Event
+                {
+                    Title = "Past Event 4",
+                    Date = DateTime.UtcNow.AddMonths(-1),
+                    Description = "Old event",
+                    Category = "drinks",
+                    City = "Stockholm",
+                    Venue = "Pub"
+                },
+                new Event
+                {
+                    Title = "Past Event 5",
+                    Date = DateTime.UtcNow.AddMonths(-2),
+                    Description = "Old event",
+                    Category = "drinks",
+                    City = "Amsterdam",
+                    Venue = "Pub"
+                },
+                new Event
+                {
+                    Title = "Past Event 6",
+                    Date = DateTime.UtcNow.AddMonths(-1),
+                    Description = "Old event",
+                    Category = "drinks",
+                    City = "Madrid",
+                    Venue = "Pub"
+                },
+            };
+            await context.Events.AddRangeAsync(events);
+            await context.SaveChangesAsync();
+        }
+    }
+}
