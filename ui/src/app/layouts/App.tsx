7752a0a6be35f6bd900f2627468479bfd549cb6a
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2023-03-24 14:35:54 +0100

    Cleanup

--- ui/src/app/layouts/App.tsx
+++ ui/src/app/layouts/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
