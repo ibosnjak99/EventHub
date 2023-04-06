730a640410c37de32cef1bb65536b4cbe9731ef4
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2023-04-06 15:45:37 +0200

    Prepared for routing, refactoring, ui styling

--- ui/src/features/events/dashboard/EventsDashboard.tsx
+++ ui/src/features/events/dashboard/EventsDashboard.tsx
@@ -1,19 +1,12 @@
 import React from "react"
 import { Grid } from "semantic-ui-react"
-import { Event } from "../../../app/models/event"
 import EventsForm from "../form/EventForm"
 import EventDetails from "./EventDetails"
 import EventsList from "./EventsList"
 import { useStore } from "../../../app/stores/store"
 import { observer } from "mobx-react-lite"
 
-interface Props {
-    events: Event[]
-    deleteEvent: (id: string) => void
-    submitting: boolean
-}
-
-export default observer (function EventsDashboard({events, deleteEvent, submitting}: Props) {
+export default observer (function EventsDashboard() {
     const {eventStore} = useStore()
     const {selectedEvent, editMode} = eventStore
 
