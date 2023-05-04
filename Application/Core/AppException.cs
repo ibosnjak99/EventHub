865ff0c99d397c4c44ef39c6b201c39ea59e4eb4
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2023-05-04 12:12:00 +0200

    Backend error handling

---
+++ Application/Core/AppException.cs
@@ -0,0 +1,25 @@
+﻿namespace Application.Core
+{
+    /// <summary>
+    /// App exception class.
+    /// </summary>
+    public class AppException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppException" /> class.
+        /// </summary>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="details">The details.</param>
+        public AppException(int statusCode, string message, string details)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+    }
+}
--- Domain/Models/Event.cs
+++ Domain/Models/Event.cs
@@ -8,12 +8,60 @@ namespace Domain
     /// <seealso cref="IEvent" />
     public class Event : IEvent
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
         public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
+        /// <value>
+        /// The date.
+        /// </value>
         public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the category.
+        /// </summary>
+        /// <value>
+        /// The category.
+        /// </value>
         public string Category { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;  
+
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
+        public string City { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the venue.
+        /// </summary>
+        /// <value>
+        /// The venue.
+        /// </value>
         public string Venue { get; set; } = string.Empty; 
     }
 }
