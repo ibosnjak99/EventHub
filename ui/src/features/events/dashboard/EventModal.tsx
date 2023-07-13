77b0ec7f0b0d5d6b2db92606a2c84a72513fb157
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2023-07-13 16:07:01 +0200

    Login and register completed

--- ui/src/features/events/dashboard/EventModal.tsx
+++ ui/src/features/events/dashboard/EventModal.tsx
@@ -39,7 +39,7 @@ export default function EventDetails() {
                 <Modal.Header>{event.title}</Modal.Header>
                 <Modal.Content scrolling>
                     <Card fluid>
-                        <Image src={`/assets/categoryImages/${event.category}.jpg`} />
+                        <Image src={`/assets/categoryImages/${event.category}.jpg`} centered />
                         <Card.Content>
                         <Segment
                             textAlign='center'
