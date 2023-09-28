9d8a8d25768c22f48f4e87dc40085f84b1a7700e
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2023-09-28 18:27:59 +0200

    Scroll back to top when redirecting

--- ui/src/app/layouts/App.tsx
+++ ui/src/app/layouts/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import {  Container } from 'semantic-ui-react'
 import NavBar from './NavBar'
 import { observer } from 'mobx-react-lite'
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { useStore } from '../stores/store'
 import LoadingComponent from './LoadingComponent'
@@ -10,7 +10,11 @@ import ModalContainer from '../common/modals/ModalContainer'
 
 function App() {
   const { commonStore, userStore } = useStore()
-
+  
+  if ('scrollRestoration' in window.history) {
+    window.history.scrollRestoration = 'manual';
+  }
+  
   useEffect(() => {
     if (commonStore.token) {
       userStore.getUser().finally(() => commonStore.setAppLoaded())
@@ -23,6 +27,7 @@ function App() {
 
   return (
     <>
+    <ScrollRestoration />
     <ModalContainer />
     <ToastContainer position='bottom-right' theme='colored' autoClose={2000} />
       <NavBar />
